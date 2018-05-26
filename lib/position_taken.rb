e3dd8570ed5c901c0d645e4bbfc6765b0e718312
Roberto Gaitan <rgaitan.it@gmail.com>
2018-05-26 21:40:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index_number)
 if board[index_number] == " " or board[index_number] == "" or board[index_number] == nil
-  true
-else
   false
+else
+  true
 end
 end
